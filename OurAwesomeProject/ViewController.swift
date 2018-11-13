8a281d166e08bc46bc2bb1787446dc3b0cd3d554
Porter Frazier <porterfrazier@Porters-MacBook-Pro.local>
2018-11-13 10:51:57 -0700

    iniitial commit

---
+++ OurAwesomeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OurAwesomeProject
+//
+//  Created by Porter Frazier on 11/13/18.
+//  Copyright © 2018 BULB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
