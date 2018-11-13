598c3843b846fc8e197c3ca1bd3fb5825f483175
Arkin Hill <ahill@luminpublishing.com>
2018-11-13 15:55:43 -0700

    Second round of changes to model controller

--- OurAwesomeProject/Model Controller/SquareController.swift
+++ OurAwesomeProject/Model Controller/SquareController.swift
@@ -15,10 +15,8 @@ class SquareController {
     // Shared
     static let shared = SquareController()
     
-    // Source
-    var squares = [Square]
-    
-    static var squares: [Square] {
+    // Source of truth
+    lazy var squares: [Square] = {
         let a1 = Square(value: "")
         let a2 = Square(value: "")
         let a3 = Square(value: "")
@@ -28,6 +26,46 @@ class SquareController {
         let c1 = Square(value: "")
         let c2 = Square(value: "")
         let c3 = Square(value: "")
-        return []
+        
+        return[a1, a2, a3, b1, b2, b3, c1, c2, c3]
+    }()
+    
+    // MARK: - FUNCTIONS
+    
+    func squareTapped () {
+        
+        // Determine currentPlayer
+        
+        // Exit function if tapped square already has value
+        
+        // Change value of tappedSquare to value of currentPlayer
+        
+    }
+        
+    func isThereAWinner () {
+        
+        // Check the eight possible trios to see if any are equal
+        
+        // A1 = A2 = A3
+        // B1 = B2 = B3
+        // C1 = C2 = C3
+        // A1 = B1 = C1
+        // A2 = B2 = C2
+        // A3 = B3 = C3
+        // A1 = B2 = C3
+        // C2 = B2 = C1
+        
+        // If there is an equal trio, declare a winner
+        
+        // if all squares have a value, call the game as a tie
+        
+        // Otherwise toggle player
+    }
+    
+    func clearBoard () {
+        
+        // Clear VC
+        
+        // Reset player and square values
     }
 }
