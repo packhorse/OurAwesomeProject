4471e5583156a70ec26e95658849386d3eb11a9f
Porter Frazier <porterfrazier@Porters-MacBook-Pro.local>
2018-11-14 16:58:45 -0700

    Updated code kind of

--- OurAwesomeProject/View Controller /ViewController.swift
+++ OurAwesomeProject/View Controller /ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     //Player 1 and player 2 background colors.
     @IBOutlet weak var player1RedView: UIView!
     @IBOutlet weak var player2BlueView: UIView!
@@ -29,7 +29,53 @@ class ViewController: UIViewController {
         player1RedView.layer.cornerRadius = 20
         player2BlueView.layer.cornerRadius = 20
         clearAll()
-   
+        GameController.shared.startGame()
+    }
+    
+    
+    
+    @IBAction func A1ButtonTapped(_ sender: UIButton) {
+        squareTapped(button: sender)
+        
+    }
+    
+    @IBAction func A2ButtonTapped(_ sender: UIButton) {
+        squareTapped(button: sender)
+        
+    }
+    
+    @IBAction func A3ButtonTapped(_ sender: UIButton) {
+        squareTapped(button: sender)
+    }
+    
+    @IBAction func B1ButtonTapped(_ sender: UIButton) {
+        squareTapped(button: sender)
+        
+    }
+    
+    @IBAction func B2ButtonTapped(_ sender: UIButton) {
+        squareTapped(button: sender)
+       
+    }
+    
+    @IBAction func B3ButtonTapped(_ sender: UIButton) {
+        squareTapped(button: sender)
+        
+    }
+    
+    @IBAction func C1ButtonTapped(_ sender: UIButton) {
+        squareTapped(button: sender)
+        
+    }
+    
+    @IBAction func C2ButtonTapped(_ sender: UIButton) {
+        squareTapped(button: sender)
+        
+    }
+    
+    @IBAction func C3ButtonTapped(_ sender: UIButton) {
+        squareTapped(button: sender)
+        
     }
     
     func clearAll(){
@@ -44,11 +90,44 @@ class ViewController: UIViewController {
         C3Square.setTitle("", for: .normal)
     }
     
+    func squareTapped(button: UIButton) {
+        // Exit function if tapped square already has value
+        if let buttonText = button.titleLabel?.text {
+            return
+        } else {
+            
+            // Track whether current player is "X" or "O"
+            guard let team = GameController.shared.currentPlayer?.team,
+                let squareLocation = button.restorationIdentifier
+                else {return}
+            
+            // Assign current team ("X" or "O") to dictionary key that matches button tapped
+            GameController.shared.squareValueChanged(squareLocation, team: team)
+            
+            // Change title of button of the square to match current player team ("X" or "O")
+            button.setTitle("\(team)", for: .normal)
+            
+        }
+        
+        GameController.shared.isThereAWinner()
+        
+        
+//        GameController.shared.togglePlayer(game:)
+    }
     
     
     
     @IBAction func clearTapped(_ sender: Any) {
-        clearAll()
+        GameController.shared.clearBoardValues()
+        A1Square.titleLabel?.text = nil
+        A2Square.titleLabel?.text = nil
+        A3Square.titleLabel?.text = nil
+        B1Square.titleLabel?.text = nil
+        B2Square.titleLabel?.text = nil
+        B3Square.titleLabel?.text = nil
+        C1Square.titleLabel?.text = nil
+        C2Square.titleLabel?.text = nil
+        C3Square.titleLabel?.text = nil
     }
     
     
